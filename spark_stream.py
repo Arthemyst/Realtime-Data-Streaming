b299377bcde607e42007b668ec56560e249c5d03
Arthemyst <pawcio262@gmail.com>
2024-06-04 18:18:22 +0200

    created content inside create_keyspace, create_table, create_cassandra_connection and insert_data functions.

--- spark_stream.py
+++ spark_stream.py
@@ -1,24 +1,63 @@
 import logging
-from datetime import datetime
 
-from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+
 
 def create_keyspace(session):
-    # create keyspace here
-    pass
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS spark_streams
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+    """)
+
+    print("Keyspace created successfully!")
 
 
 def create_table(session):
-    # create table here
-    pass
+    session.execute("""
+    CREATE TABLE IF NOT EXISTS spark_streams.created_users (
+        id UUID PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        address TEXT,
+        post_code TEXT,
+        email TEXT,
+        username TEXT,
+        registered_date TEXT,
+        phone TEXT,
+        picture TEXT
+    """)
+
+    print("Table created successfully!")
 
 
 def insert_data(session, **kwargs):
-    # insertion here
-    pass
+    print("inserting data...")
+
+    user_id = kwargs.get('id')
+    first_name = kwargs.get('first_name')
+    last_name = kwargs.get('last_name')
+    gender = kwargs.get('gender')
+    address = kwargs.get('address')
+    postcode = kwargs.get('postcode', 'no postcode')
+    email = kwargs.get('email')
+    username = kwargs.get('username')
+    dob = kwargs.get('dob')
+    registered_date = kwargs.get('registered_date')
+    phone = kwargs.get('phone')
+    picture = kwargs.get('picture', 'no picture')
+
+    try:
+        session.execute("""
+            INSERT INTO spark_streams.created_users(user_id, first_name, last_name, gender, address, 
+            postcode, email, username, dob, registered_date, phone, picture)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (user_id, first_name, last_name, gender, address,
+              postcode, email, username, dob, registered_date, phone, picture))
+        logging.info(f"Data inserted for {first_name} {last_name}")
+    except Exception as e:
+        logging.error(f'Could not insert data due to exception: {e}')
 
 
 def create_spark_connection():
@@ -39,9 +78,22 @@ def create_spark_connection():
 
 
 def create_cassandra_connection():
-    # creating cassandra connection
-    pass
+    cassandra_session = None
+    try:
+        cluster = Cluster(['localhost'])
+        cassandra_session = cluster.connect()
+    except Exception as e:
+        logging.error(f'Could not create cassandra connection due to exception: {e}')
+
+    return cassandra_session
 
 
 if __name__ == "__main__":
     spark_connection = create_spark_connection()
+
+    if spark_connection is not None:
+        session = create_cassandra_connection()
+
+        if session:
+            create_keyspace(session)
+            create_table(session)
